87ca29a1843a8e8d29397a549de3cffe22b58c4a
mohit  Kumar Sharma <kaushikmohit54@hotmail.com>
2018-09-24 17:52:03 +0530

    3rd commit with login detials

---
+++ Automation_Test/src/test/java/Pages/Login.java
@@ -0,0 +1,13 @@
+package Pages;
+
+import org.testng.annotations.Test;
+
+public class Login extends BaseTest {
+	
+	@Test
+	public void login1() {
+		driver.get("https://www.mailinator.com");
+			
+	}
+
+}
